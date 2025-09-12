f784475947a9b44c25bc38cef51fcab9b60fc1ef
Stuart-Dann <stuartdann03@gmail.com>
2025-09-12 23:19:31 +0100

    Added way to admin dashboard admin sign in, favorites system, applied system, adjusted css

---
+++ src/components/resultsTable.jsx
@@ -0,0 +1,67 @@
+// components/ResultsTable.jsx
+import FavouriteStar from "./favouriteStar";
+import './resultsTable.css';
+import { addToFavourites } from "../services/favourite";
+import { useState, useEffect } from "react";
+import openInNew from '../assets/open-in-new.svg';
+
+export default function ResultsTable({ programs: initialPrograms }) {
+    const [programs, setPrograms] = useState(initialPrograms || []);
+
+    useEffect(() => {
+    if (initialPrograms) {
+        setPrograms(initialPrograms);
+        }
+    }, [initialPrograms]);
+
+    const updateStatus = (id) => {
+    setPrograms(programs.map(prog =>
+        prog.id === id ? { ...prog, status: "Applied" } : prog
+    ));
+    let applied = JSON.parse(localStorage.getItem('Applied')) || [];
+    if (!applied.includes(id)) {
+        applied.push(id);
+        localStorage.setItem('Applied', JSON.stringify(applied));
+    }
+};
+
+    return (
+        <div className="results-inner">
+        <table className="results-table">
+            <thead>
+            <tr>
+                <th></th>
+                <th>Status</th>
+                <th>Program Name</th>
+                <th>Company Name</th>
+                <th>Location</th>
+                <th>Subject</th>
+                <th>Closing Date</th>
+            </tr>
+            </thead>
+            <tbody>
+            {programs.map((program) => (
+                <tr key={program.id}>
+                <td>
+                    <FavouriteStar
+                    initial={program.isFavourite}
+                    onToggle={() => addToFavourites(program.id)}
+                    />
+                </td>
+                <td>{program.status}</td>
+                <td><a href={program.href}  onClick={() => {
+                    updateStatus(program.id, setPrograms);
+                }} target="_blank"
+                >
+                    {program.program}<img src={openInNew} alt="Open in new" style={{ width: '1em', height: '1em', verticalAlign: 'middle' }} /></a></td>
+                <td>{program.company}</td>
+                <td>{program.location}</td>
+                <td>{program.subject}</td>
+                <td>{program.closingDate}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        </div>
+    );
+}
--- src/pages/searchPage.jsx
+++ src/pages/searchPage.jsx
@@ -1,47 +1,81 @@
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 import './searchPage.css';
+import ResultsTable from '../components/resultsTable';
+import FilterGroup from '../components/filterGroups';
+import useIsMobile from '../hooks/isMobile.js';
+import { useEffect, useState } from 'react';
+import { getInternships, subscribeToItems } from '../services/firestore';
 
-export default function searchPage() {
+export default function SearchPage() {
+    const isMobile = useIsMobile();
+
+    const [programs, setPrograms] = useState([]);
+
+    useEffect(() => {
+    // One-time fetch
+        getInternships().then(setPrograms);
+
+        // Real-time updates
+        const unsubscribe = subscribeToItems(setPrograms);
+        return unsubscribe;
+    }, []);
+
+    const favs = JSON.parse(localStorage.getItem('favourites')) || [];
+    programs.forEach(prog => {
+        if (favs.includes(prog.id)) {
+            prog.isFavourite = true;
+        }
+    });
+    const applied = JSON.parse(localStorage.getItem('Applied')) || [];
+    programs.forEach(prog => {
+        if (applied.includes(prog.id)) {
+            prog.status = "Applied";
+        } else {
+            prog.status = "Not Applied";
+        }
+    });
+
+    const filters = [
+        {
+            title: "Location",
+            options: [
+            { value: "usa", label: "USA", count: 20 },
+            { value: "uk", label: "UK", count: 8 },
+            ],
+        },
+        {
+            title: "Subject",
+            options: [
+            { value: "physics", label: "Physics", count: 20 },
+            { value: "chemistry", label: "Chemistry", count: 8 },
+            ],
+        },
+        {
+            title: "Company",
+            options: [
+            { value: "ukaea", label: "UKAEA", count: 20 },
+            { value: "lidl", label: "Lidl", count: 8 },
+            ],
+        },
+        ];
+    
     return (
         <div className='search-page'>
             <Navbar />
             <div id='search-page-body'>
-                <div id='filter-body'>
-                    <p className='filter-name'>Country</p>
-                    <div className='filters'>
-                        <div className='filter'>
-                            <div className='filter-details'>
-                                <input type="checkbox" id="usa" name="usa" value="USA" />
-                                <label htmlFor="usa">USA</label>
-                            </div>
-                            <p className='filter-stats'>20</p>
+                <div id="filter-body">
+                    {isMobile ? (
+                        <div className="mobile-filters">
+                        {filters.map((filter) => (
+                            <FilterGroup key={filter.title} data={{ ...filter, isMobile }} />
+                        ))}
                         </div>
-                        <div className='filter'>
-                            <div className='filter-details'>
-                                <input type="checkbox" id="uk" name="uk" value="UK" />
-                                <label htmlFor="uk">UK</label>
-                            </div>
-                            <p className='filter-stats'>8</p>
-                        </div>
-                    </div>
-                    <p className='filter-name'>Subject</p>
-                    <div className='filters'>
-                        <div className='filter'>
-                            <div className='filter-details'>
-                                <input type="checkbox" id="physics" name="physics" value="Physics" />
-                                <label htmlFor="physics">Physics</label>
-                            </div>
-                            <p className='filter-stats'>20</p>
-                        </div>
-                        <div className='filter'>
-                            <div className='filter-details'>
-                                <input type="checkbox" id="chemistry" name="chemistry" value="Chemistry" />
-                                <label htmlFor="chemistry">Chemistry</label>
-                            </div>
-                            <p className='filter-stats'>8</p>
-                        </div>
-                    </div>
+                    ) : (
+                        filters.map((filter) => (
+                        <FilterGroup key={filter.title} data={{ ...filter, isMobile }} />
+                        ))
+                    )}
                 </div>
                 <div id='search-results'>
                     <div id='search-body'>
@@ -62,34 +96,9 @@ export default function searchPage() {
                             <label htmlFor='open-programs'>Show Only Open Programs</label>
                         </div>
                     </div>
-                        <div id='results-container'>
-                            <div id='result-header'>
-                                <p className='result-header-item' id='status'>Status</p>
-                                <p className='result-header-item' id='program-name'>Program Name</p>
-                                <p className='result-header-item' id='company-name'>Company Name</p>
-                                <p className='result-header-item' id='location'>Location</p>
-                                <p className='result-header-item' id='subject'>Subject</p>
-                                <p className='result-header-item' id='closing-date'>Closing Date</p>
-                            </div>
-                            <div id='results'>
-                                <div className='result'>
-                                    <p className='result-item' id='status'>Applied</p>
-                                    <p className='result-item' id='program-name'>Business Analyst Intern</p>
-                                    <p className='result-item' id='company-name'>Goldman Sachs</p>
-                                    <p className='result-item' id='location'>USA</p>
-                                    <p className='result-item' id='subject'>Business</p>
-                                    <p className='result-item' id='closing-date'>30th June 2024</p>
-                                </div>
-                                <div className='result'>
-                                    <p className='result-item' id='status'>Applied</p>
-                                    <p className='result-item' id='program-name'>Business Analyst Intern</p>
-                                    <p className='result-item' id='company-name'>Goldman Sachs</p>
-                                    <p className='result-item' id='location'>USA</p>
-                                    <p className='result-item' id='subject'>Business</p>
-                                    <p className='result-item' id='closing-date'>30th June 2024</p>
-                                </div>
-                            </div>
-                        </div>
+                    <div id="results-container">
+                        <ResultsTable programs={programs} />
+                    </div>
                 </div>
             </div>
             <Footer />
